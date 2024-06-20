945815ef64cfbe6a0f00415c1801d90f91f257f0
Zack_Z <kressety@163.com>
2024-06-21 00:11:26 +0100

    Removed client setting

--- server_config.py
+++ server_config.py
@@ -8,15 +8,11 @@ def update_json(file_path, updates):
         json.dump(data, file, indent=4)
 
 settings_path = 'YgoMaster/Data/Settings.json'
-client_settings_path = 'YgoMaster/Data/ClientSettings.json'
 
 update_json(settings_path, {
     'MultiplayerEnabled': True,
     'SessionServerIP': '0.0.0.0',
     'MultiplayerPvpClientConnectIP': 'localhost',
     'BindIP': 'http://*:{BasePort}/',
-    'BaseIP': '0.0.0.0'
-})
-update_json(client_settings_path, {
     'BaseIP': '0.0.0.0'
 })
